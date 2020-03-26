ee734672dec47ec32594773547045456a9726d4a
rayhan <techierayhan@gmail.com>
2020-03-26 15:02:45 +0600

    Toast

--- app/src/main/java/com/mycompany/fight/MainActivity.java
+++ app/src/main/java/com/mycompany/fight/MainActivity.java
@@ -2,6 +2,10 @@ package com.mycompany.fight;
 
 import android.app.*;
 import android.os.*;
+import android.view.View;
+import android.widget. *;
+import android.widget.Button.*;
+import android.widget.SearchView;
 
 public class MainActivity extends Activity 
 {
@@ -10,5 +14,17 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
+   
+		Button bb=(Button)findViewById(R.id.my_button);
+		
+		bb.setOnClickListener(new View.OnClickListener(){
+			public void onClick(View view){
+				Toast.makeText(getApplicationContext(), "Well done!",  
+							   Toast.LENGTH_LONG).show();
+
+			}
+		});
+		
+		
+		}
 }
